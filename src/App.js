653a1e9201e5051609b88ac7b1590f105e584cd2
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-10-04 16:39:59 +0900

    Feat : test를 위한  App.js 작성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg'; 
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [hidata, setHello] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/hello')
+      .then(response => setHello(response.data))
+      .catch(error => console.log(error))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      백엔드 스프링 부트 데이터 : {hidata}
     </div>
   );
 }
