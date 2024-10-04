34d75b5d59e428a1b2f6a2c45e24900aa810bf50
bada <silverzoo030@gmail.com>
2024-10-04 16:09:36 +0900

    Design : 메인페이지 로고 변경

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          편안하게 집을 편집하다 : 가구 쇼핑몰 편집
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
