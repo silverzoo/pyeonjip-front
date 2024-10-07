54d961d9349e5c6a2b01c2f6ab893a373ebaf4fc
[CLOUD4] 신영광 <halogod9985@naver.com>
2024-10-07 16:47:31 +0900

    feature/cart-sandbox - 부트스트랩 임포트 - Sandbox.js - Cart 라우팅 성공

--- src/Components/router/Router.js
+++ src/Components/router/Router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sandbox from "../cart/Sandbox";
 import Cart from "../cart/Cart";
 
 function AppRouter() {
     return (
         <Router>
             <Routes>
+                <Route path="/cart/sandbox" element={<Sandbox />} />
                 <Route path="/cart" element={<Cart />} />
             </Routes>
         </Router>
