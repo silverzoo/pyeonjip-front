c1c254bc342dd9ddb76c4fa87e5978645f93dd9c
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-10-09 10:51:25 +0900

    Design : 헤더, 푸터 라우트 추가

--- src/pages/router/index.js
+++ src/pages/router/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Sandbox from "../Cart/Sandbox";
 import Cart from "../Cart/index";
+import Home from "../Home/home";
+import AdminCategory from "../Admin/category";
+import AdminOrder from "../Admin/order";
 
 function AppRouter() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/cart/sandbox" element={<Sandbox />} />
-                <Route path="/cart" element={<Cart />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/cart/sandbox" element={<Sandbox />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/admin/category" element={<AdminCategory />} />
+            <Route path="/admin/order" element={<AdminOrder />} />
+        </Routes>
     );
 }
 
