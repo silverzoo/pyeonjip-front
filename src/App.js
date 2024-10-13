20d69e97c2f9d5fdada56eb298092215126de506
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-10-14 01:53:08 +0900

    Style : 파일명 수정

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import AppRouter from './pages/router';
 import Footer from './components/Footer/Footer';
-import LeftSidebar from './components/LeftSidebar/LeftSidebar';
+import LeftSide from './components/LeftSide/LeftSide';
 import SidePanel from './components/SidePanel/sidePanel'
 
 function App() {
   return (
       <div className="app">
-        <div className='leftSidebar'>
-          <LeftSidebar />
+        <div className='leftSide'>
+          <LeftSide />
         </div>
         <div className='sidePannel'>
             <SidePanel />
