f4671ce5b8ec5db21be53f1bee556fafe23cb509
[CLOUD4] 신영광 <halogod9985@naver.com>
2024-10-15 00:15:13 +0900

    Feat/Cart - Home 페이지

--- src/pages/router/Router.js
+++ src/pages/router/Router.js
@@ -3,19 +3,23 @@ import { Route, Routes } from 'react-router-dom';
 import Sandbox from "../Product/Product";
 import ProductDetail from "../Product/ProductDetail";
 import Cart from "../Cart/Cart";
-import Home from "../Home/Home";
-import AdminCategory from "../Admin/category";
-import AdminOrder from "../Admin/Order";
+import Home from "../Home/home";
+import AdminCategory from "../Admin/Category/category";
+import AdminOrder from "../Admin/Order/Order";
 import Login from "../User/login";
 import Signup from '../User/signup';
 import Logout from '../User/logout';
 import OrderPage from '../Order/OrderPage';
+import Main from "../Product/Main";
+import ChatPage from '../Chat/chatPage';
+import ProductAdmin from '../Admin/ProductAdmin';
+
 
 function AppRouter() {
     return (
         <Routes>
+            <Route path="/" element={<Main />} />
             <Route path="/category/:categoryId" element={<Sandbox />} />
-            <Route path="/shop" element={<Sandbox />} />
             <Route path="/product-detail" element={<ProductDetail />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/" element={<Home />} />
@@ -25,8 +29,8 @@ function AppRouter() {
             <Route path="/logout" element={<Logout />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/order" element={<OrderPage />} />
+            <Route path="/chat" element={<ChatPage />} />
+            <Route path="/admin/products" element={<ProductAdmin />} />
         </Routes>
     );
 }
-
-export default AppRouter;
