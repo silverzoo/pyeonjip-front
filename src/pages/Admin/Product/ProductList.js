c98ed62d4d8e1828a9343da765ceeaa36bf4782a
[CLOUD4] 최병욱 <1chlquddnr1@naver.com>
2024-10-17 01:24:55 +0900

    Refactor: 상품 생성 페이지 버튼 생성

--- src/pages/Admin/Product/ProductList.js
+++ src/pages/Admin/Product/ProductList.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ProductItem from './ProductItem';
+import { useNavigate } from 'react-router-dom';
 
 function ProductList({ products, selectedProducts, handleCheckboxChange, handleBulkDelete }) {
+    const navigate = useNavigate(); // navigate 훅 사용
+
+    // 상품 생성 페이지로 이동하는 함수
+    const navigateToCreateProduct = () => {
+        navigate('/admin/createproduct'); // 원하는 경로로 이동
+    };
     return (
         <div className="product-list">
             <h2 className="mb-4">상품 목록</h2>
@@ -23,6 +30,12 @@ function ProductList({ products, selectedProducts, handleCheckboxChange, handleB
                         disabled={selectedProducts.length === 0}
                     >
                         선택된 상품 삭제
+                    </button>
+                    <button
+                        className="btn-black mt-3"
+                        onClick={navigateToCreateProduct}
+                    >
+                        상품 생성
                     </button>
                 </div>
             ) : (
