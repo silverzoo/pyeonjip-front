e6782f240578c480ba452ce77755c91ae023cec3
jsjhyun <gusrnl98@naver.com>
2024-10-17 14:41:58 +0900

    feat: 장바구니, 유저 데이터 불러오기

--- src/pages/Order/OrderPage.js
+++ src/pages/Order/OrderPage.js
@@ -1,43 +1,107 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import './OrderPage.css'; 
 
 function OrderPage() {
-    const [orderData, setOrderData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const userId = 1; // 실제 로그인된 사용자 ID로 변경 필요
-    const cartDetails = JSON.parse(localStorage.getItem('cart')) || []; // 로컬 스토리지에서 카트 정보 가져오기
+    const location = useLocation();
+    const navigate = useNavigate();
+    const orderData = location.state?.orderSummary;
 
+    const [userId, setUserId] = useState('');
+    const [recipient, setRecipient] = useState(''); // 수령인
+    const [phoneNumber, setPhoneNumber] = useState(''); // 연락처
+    const [address, setAddress] = useState(''); // 배송지 
+    const [requirement, setRequirement] = useState(''); // 요구사항
+    const [grade, setGrade] = useState(''); // 회원 등급
+    const [loading, setLoading] = useState(true); // 로딩
+    const [error, setError] = useState(''); // 에러
+
+    // 사용자 정보 가져오기
     useEffect(() => {
-        const fetchOrderData = async () => {
+        const fetchUserData = async () => {
+          const userId = 1;
             try {
-                // POST 요청으로 데이터를 백엔드로 전송
-                const response = await fetch('http://localhost:8080/api/orders/checkout', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        userId: userId,
-                        orderDetails: cartDetails,
-                    }),
-                });
-
+                const response = await fetch(`http://localhost:8080/api/user/${userId}`);
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
+                const userData = await response.json();
 
-                const data = await response.json();
-                setOrderData(data);
+                // 유저 정보 설정
+                setUserId(1);
+                setRecipient(userData.name);
+                setPhoneNumber(userData.phoneNumber);
+                setAddress(userData.address);
+                setGrade(userData.grade);
+
+                // 로딩 상태 업데이트
                 setLoading(false);
             } catch (error) {
-                console.error('데이터를 가져오는 중 오류 발생:', error);
+                console.error('데이터를 가져오는 중 오류 발생:', error.message);
                 setError('데이터를 가져오는 중 오류가 발생했습니다.');
                 setLoading(false);
             }
         };
 
-        fetchOrderData();
+        fetchUserData();
     }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!recipient || !phoneNumber || !address) {
+            alert('모든 필드를 입력해 주세요.');
+            return;
+        }
+
+        try {
+          // 주문 데이터를 DTO에 맞춰 구성
+          const orderRequestData = {
+            recipient: recipient,
+            phoneNumber: phoneNumber,
+            address: address,
+            requirement: requirement,
+            orderDetails: orderData.orderDetail, // 주문 상세 정보를 포함
+        };
+
+            const orderCartData = {
+                userId: 1,
+                cartTotalPrice: orderData.cartTotalPrice,
+                orderDetails: orderData.orderDetail,
+            };
+
+                  const combinedOrderData = {
+                    orderRequestDto: orderRequestData,
+                    orderCartRequestDto: orderCartData,
+                    userId: 1,
+                };
+
+      const response = await fetch(`http://localhost:8080/api/orders?userId=${userId}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(combinedOrderData),    
+    });
+          console.log('응답 상태:', response.status);
+  
+          if (response.ok) {
+              // console.log('주문이 성공적으로 완료되었습니다.');
+              navigate('/order-success');
+          } else {
+            const errorData = await response.json();
+            console.error('서버 오류:', errorData);
+            alert('주문에 실패했습니다. 다시 시도해 주세요.');
+          }
+      } catch (error) {
+          console.error('주문 처리 중 오류 발생:', error);
+          alert('주문 처리 중 오류가 발생했습니다.');
+      }
+    };
+
+    const handleBackToCart = () => {
+        navigate('/cart'); // 돌아가기 버튼 클릭 시 장바구니 페이지로 이동
+    };
 
     if (loading) {
         return <div>로딩 중...</div>;
@@ -47,27 +111,123 @@ function OrderPage() {
         return <div>{error}</div>;
     }
 
+    if (!orderData) {
+        return <div>주문 데이터가 없습니다.</div>;
+    }
+
+    console.log(orderData);
+
     return (
-        <div>
-            <h2>주문 페이지</h2>
-            <div>
-                <h3>총 결제 금액</h3>
-                <p>₩{orderData.totalPrice}</p>
-                <h3>할인 후 가격</h3>
-                <p>₩{orderData.disCountedTotalPrice}</p>
-                <h3>쿠폰 할인율</h3>
-                <p>{orderData.couponDiscountRate}%</p>
-                <h3>등급에 따른 배송비</h3>
-                <p>₩{orderData.deliveryPrice}</p>
-                <h3>상품 목록</h3>
-                <ul>
-                    {orderData.orderDetails.map((item, index) => (
-                        <li key={index}>
-                            {item.productName} - ₩{item.productPrice} x {item.quantity}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+        <div className="order-page">
+            <main className="content">
+                {/* Product List */}
+                <section className="product-list">
+                    <h6 className="section-title">상품목록</h6>
+                    {orderData.orderDetail && orderData.orderDetail.length > 0 ? (
+                        <ul>
+                        {orderData.orderDetail.map((item, index) => (
+                            <li key={index} className="product-item">
+                                <img
+                                    src={item.productImage}
+                                    alt={item.productName}
+                                    className="product-image"
+                                />
+                                <div className="product-details">
+                                    <div className="product-name">{item.productName}</div>
+                                    <div className="product-price">₩{item.productPrice}</div>
+                                </div>
+                                <div className="product-summary">
+                                    <div className="product-quantity">수량: {item.quantity}</div>
+                                    <div className="subtotal-price"></div>
+                                      ₩{(item.quantity * item.productPrice)}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                    ) : (
+                        <li>상품 정보가 없습니다.</li>
+                    )}
+                </section>
+
+                {/* 회원 등급 */}
+                <section className="membership">
+                    <h6>회원 등급: {grade}</h6>
+                </section>
+
+                {/* 상품 정보 */}
+                <section className="order-summary">
+                    <div className="order-summary-left">
+                        <h6>총 결제 금액</h6>
+                    </div>
+                    <div className="order-summary-right">
+                        <div className="price-summary-item">
+                            <span className="price-summary-label">쿠폰 적용 후 가격</span>
+                            <span>₩{orderData.cartTotalPrice}</span>
+                        </div>
+                        <div className="price-summary-item">
+                            <span className="price-summary-label">배송비</span>
+                            <span>₩{orderData.deliveryPrice}</span>
+                        </div>
+                        <div className="price-summary-item">
+                            <span className="price-summary-label">할인율</span>
+                            <span>{orderData.discountRate * 100}%</span>
+                        </div>
+                        <div className="total-price">
+                            <span>=</span>
+                            <span className="final-price">₩{orderData.totalPrice}</span>
+                        </div>
+                    </div>
+                </section>
+
+                {/* 배송 정보 */}
+                <section className="delivery-info">
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            수령인
+                            <input
+                                type="text"
+                                name="name"
+                                value={recipient}
+                                onChange={(e) => setRecipient(e.target.value)}
+                            />
+                        </label>
+                        <label>
+                            연락처
+                            <input
+                                type="text"
+                                name="phoneNumber"
+                                value={phoneNumber}
+                                onChange={(e) => setPhoneNumber(e.target.value)}
+                            />
+                        </label>
+                        <label>
+                            배송지 주소
+                            <input
+                                type="text"
+                                name="address"
+                                value={address}
+                                onChange={(e) => setAddress(e.target.value)}
+                            />
+                        </label>
+                        <label>
+                            주문요청사항
+                            <textarea
+                                name="requirement"
+                                value={requirement}
+                                onChange={(e) => setRequirement(e.target.value)}
+                            ></textarea>
+                        </label>
+                        <div className="actions">
+                            <button type="button" className="order-back-button" onClick={handleBackToCart}>
+                                돌아가기
+                            </button>
+                            <button type="submit" className="order-submit-button">
+                                주문하기
+                            </button>
+                        </div>
+                    </form>
+                </section>
+            </main>
         </div>
     );
 }
