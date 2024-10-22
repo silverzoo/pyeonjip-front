4cf249ddadf11149dce8ec290ca8ad290c5f2448
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-10-22 19:56:56 +0900

    Design : 어드민 주문관리 UI 수정

--- src/pages/Admin/Order/OrderItem.jsx
+++ src/pages/Admin/Order/OrderItem.jsx
@@ -8,8 +8,15 @@ function OrderItem({ order, onDelete }) {
     const [selectedDeliveryStatus, setSelectedDeliveryStatus] = useState(order.deliveryStatus);
 
     const formatDate = (dateString) => {
-        const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-        return new Date(dateString).toLocaleDateString(undefined, options).replace(/\//g, '-');
+        const date = new Date(dateString);
+        const options = {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour12: false,
+            timeZone: 'Asia/Seoul'
+        };
+        return new Intl.DateTimeFormat('ko-KR', options).format(date);
     };
 
     useEffect(() => {
@@ -57,42 +64,49 @@ function OrderItem({ order, onDelete }) {
     return (
         <>
             <li className="admin-order-item">
+                <span onClick={toggleDetails} style={{ color: '#343a40', fontWeight: 'bold', fontSize: '14px'}}>{order.id}</span>
                 <span onClick={toggleDetails}>{order.userName}</span>
                 <span onClick={toggleDetails}>{order.userEmail}</span>
+                <span onClick={toggleDetails}>{order.phoneNumber}</span>
                 <span onClick={toggleDetails}>{order.orderStatus}</span>
-                <span onClick={toggleDetails}>{order.totalPrice.toLocaleString()}원</span>
                 <span onClick={toggleDetails}>{formatDate(order.createdAt)}</span>
                 <span>
-                    <select value={selectedDeliveryStatus} onChange={handleDeliveryStatusChange}>
+                    <select className='form-select' value={selectedDeliveryStatus} onChange={handleDeliveryStatusChange}>
                         <option value="READY">READY</option>
                         <option value="SHIPPING">SHIPPING</option>
                         <option value="COMPLETED">COMPLETED</option>
                         <option value="REFUNDING">REFUNDING</option>
                     </select>
                 </span>
-                <button onClick={handleDelete} disabled={isDeleting}>
-                    {isDeleting ? '삭제 중...' : '삭제'}
-                </button>
-                <button onClick={handleEdit}>
-                    수정
-                </button>
+                <span style={{ marginRight: '10px'}}>
+                    <button onClick={handleDelete} disabled={isDeleting}>
+                        {isDeleting ? '삭제 중...' : '삭제'}
+                    </button>
+                    <button onClick={handleEdit}>
+                        수정
+                    </button>
+                </span>
             </li>
             {isExpanded && (
                 <div className="admin-order-details">
                     {order.orderDetails.map(detail => (
                         <div key={detail.productDetailId} className="admin-order-detail-item">
-                            {detail.productImage && (
-                                <img src={detail.productImage} alt={detail.productName} className="product-image" />
-                            )}
                             <div className="admin-order-detail-info">
-                                <span><strong>제품명:</strong> {detail.productName}</span>
-                                <span><strong>수량:</strong> {detail.quantity}</span>
-                                <span><strong>제품 가격:</strong> {detail.productPrice.toLocaleString()}원</span>
+                                {detail.productImage && (
+                                    <img src={detail.productImage} alt={detail.productName} />
+                                )}
+                                <span style={{ marginRight: '20px' }}><strong>제품명:</strong> {detail.productName}</span>
+                                <span style={{ flexGrow: '2', marginRight: '20px'}}><strong>옵션:</strong> {detail.productDetailName}</span>
+                                <span style={{ flexGrow: '0', width: '200px'}}><strong>수량:</strong> {detail.quantity}</span>
                                 <span><strong>금액:</strong> {detail.subTotalPrice.toLocaleString()}원</span>
                             </div>
                         </div>
                     ))}
+                    <div className="admin-order-detail-totalPrice">
+                        <span><strong>총 주문 금액:</strong> {order.totalPrice.toLocaleString()}원</span>
+                    </div>
                 </div>
+
             )}
         </>
     );
