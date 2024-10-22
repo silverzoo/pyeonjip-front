55bc40e9cef52a9be4d521ddd843c78b933260d6
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-10-22 14:30:48 +0900

    fix : 버튼 클래스네임 변경

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
                 </div>
                     </CartProvider>
 
-                <div className='chatButton'>
+                <div className='chatDashboardButton'>
                     <HiddenUtils whitelist={['/chat', '/login', '/signup', '/reset-password', '/find', '/not-found']}>
                         <ChatDashboardButton/>
                     </HiddenUtils>
