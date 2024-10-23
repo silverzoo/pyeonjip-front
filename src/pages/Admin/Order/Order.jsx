8204fb9bd6288d6b831c60a8b0a32df697ffa75f
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-10-23 10:54:07 +0900

    Design : 어드민 주문관리 UI 수정

--- src/pages/Admin/Order/Order.jsx
+++ src/pages/Admin/Order/Order.jsx
@@ -105,7 +105,7 @@ function AdminOrder() {
     };
 
     return (
-        <div className="card mt-5 p-2 border rounded shadow-sm">
+        <div className="card mt-5 mb-5 p-2 border rounded shadow-sm">
             <h2 className="text-center mb-4 mt-3">주문 관리</h2>
              <div className="d-flex justify-content-between align-items-center mb-4 px-3">
                 <select value={sortOrder} onChange={handleSortChange} className="form-select w-auto">
