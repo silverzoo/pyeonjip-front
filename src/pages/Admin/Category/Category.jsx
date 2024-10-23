773818082be4c1c1fcc2002abd10268eb9700113
[CLOUD4] 이은주 <silverzoo030@gmail.com>
2024-10-23 10:54:21 +0900

    Feat : 어드민 카테고리 관리 삭제 구현

--- src/pages/Admin/Category/Category.jsx
+++ src/pages/Admin/Category/Category.jsx
@@ -1,49 +1,82 @@
+import './Category.css';
 import React, { useState, useEffect } from 'react';
-import {useAuth} from "../../../context/AuthContext";
-import {useNavigate} from "react-router-dom";
-
-export const fetchGetCategories = async () => {
-    const response = await fetch('/api/category');
-    return await response.json();
-}
+import {fetchDeleteCategory, fetchGetCategories} from "../../../api/AdminUtils";
+import CategoryItem from "./CategoryItem";
 
 function AdminCategory() {
     const [categories, setCategories] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [selectedCategoryId, setSelectedCategoryId] = useState(null);
 
     useEffect(() => {
-        const getCategories = async () => {
-            try {
-                const data = await fetchGetCategories();
-                setCategories(data);
-            } catch (err) {
-                setError(err);
-            } finally {
-                setLoading(false);
-            }
-        };
-        getCategories();
+        window.feather.replace();
     }, []);
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await fetchGetCategories();
+                console.log(response);
+                setCategories(response);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+        fetchCategories();
+    }, []);
 
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
+
+
+    const handleCategorySelect = (id) => {
+        setSelectedCategoryId(id);
+    };
+
+    const handleDelete = async () => {
+        console.log("삭제 버튼 클릭됨"); // 로그 추가
+        if (selectedCategoryId) {
+            try {
+                await fetchDeleteCategory(selectedCategoryId);
+                const updatedCategories = await fetchGetCategories();
+                setCategories(updatedCategories);
+                setSelectedCategoryId(null);
+            } catch (error) {
+                console.error(error.message);
+            }
+        } else {
+            console.log("삭제할 카테고리가 선택되지 않음"); // 로그 추가
+        }
+    };
 
     return (
-        <div>
-            <div className="admin-category-container">
-                <div>
-                    <h2>어드민 홈페이지 - 카테고리 관리</h2>
-                    <ul>
-                        {categories.map((category) => (
-                            <li key={category.id}>{category.name}</li>
-                        ))}
-                    </ul>
+        <div className="card mt-5 p-2 mb-5 border rounded shadow-sm">
+            <h2 className="text-center mb-4 mt-3">카테고리 관리</h2>
+            <span className="admin-category-btn-container" style={{marginRight: '0px'}}>
+                <button className="admin-category-btn" onClick={handleDelete}>삭제</button>
+                <button className="admin-category-btn">생성</button>
+            </span>
+            <div className="admin-category-header">
+                <span>
+                    <i data-feather="plus-circle"
+                       style={{width: '16px', height: '16px', marginTop: '-3px', marginRight: '3px'}}></i>
+                    <span style={{fontSize: '1em'}}><strong> NEW 카테고리&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong></span>
+                    <span>원하는 카테고리 수정 및 생성이 가능합니다.</span>
+                </span>
+            </div>
+            <div className="admin-category-list-container">
+                <div className="admin-category-list">
+                    <div className="admin-category-item">
+                        <CategoryItem
+                            categories={categories}
+                            hasParent={false}
+                            onCategorySelect={handleCategorySelect}
+                            selectedCategoryId={selectedCategoryId}
+                        />
+                    </div>
+                    <div className="admin-category-divider"></div>
+                    <div className="admin-category-item">
+                        <div>생성</div>
+                        <div>생성</div>
+                        <div>생성</div>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/pages/Admin/Category/Icon.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+
+function Icon({ hasChildren, hasParent }) {
+    useEffect(() => {
+        window.feather.replace();
+    }, []);
+
+    return (
+        <span>
+            {hasParent ? (
+                hasChildren ? (
+                    <i className="middle-folder">
+                        <i data-feather="more-vertical"
+                           style={{width: '10px', height: '16px', marginTop: '-3px', marginRight: '-6px'}}></i>
+                        <i data-feather="more-horizontal"
+                           style={{width: '10px', height: '16px', marginTop: '3px', marginRight: '5px'}}></i>&nbsp;
+                        <i data-feather="folder"
+                           style={{width: '16px', height: '16px', marginTop: '-3px', marginRight: '3px'}}></i>&nbsp;
+                    </i>
+                ) : (
+                    <i className="last-folder">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <i data-feather="more-vertical"
+                           style={{width: '10px', height: '16px', marginTop: '-3px', marginRight: '-5px'}}></i>
+                        <i data-feather="more-horizontal"
+                           style={{width: '10px', height: '16px', marginTop: '3px', marginRight: '5px'}}></i>&nbsp;
+                    </i>
+                )
+            ) : (
+                <i className="top-folder">
+                    <i data-feather="folder"
+                       style={{width: '16px', height: '16px', marginTop: '-3px', marginRight: '3px'}}></i>&nbsp;
+                </i>
+            )}
+        </span>
+    );
+}
+
+export default Icon;
